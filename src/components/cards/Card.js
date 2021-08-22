7447bedce4f006aa1bd1165f64d1beec35ec62c4
NurKevser <nurkevserkadiroglu@gmail.com>
2021-08-22 22:18:40 +0300

    get first element with for loop

--- src/components/cards/Card.js
+++ src/components/cards/Card.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React from "react";
 import { data } from "../../helpers/Data";
 import "./Card.css";
 
 function Card() {
+  return Datas();
+}
+
+function Datas() {
+    
+  for (let i = 0; i < data.length; i++) {
     return (
-       /*  <div className="card-container">
+     <div className="card-container">
+        <div className="cards">
+          <div className="title">
+            <h2>{data[i].title}</h2>
+          </div>
+          <img src={data[i].image} alt={data[i].title} />
+          <div className="card-over">
+            <p>{data[i].desc}</p>
+          </div>
+        </div>
+      </div>
+        
+    );
+}
+}
+
+export default Card;
+
+/*  <div className="card-container">
             <div className="cards">
                 <div className="title">
                 <h2>{data[0].title}</h2>
@@ -15,29 +39,3 @@ function Card() {
                 </div>
             </div>
         </div> */
-        Datas()
-    );
-}
-
-
-function Datas() {
-    for (let i = 0; i < data.length; i++) {
-        return (
-            `<div className="card-container">
-            <div className="cards">
-                <div className="title">
-                <h2>${data[i].title}</h2>
-                </div>
-                <img src=${data[i].image} alt=${data[i].title} />
-                <div className="card-over">
-                    <p>${data[i].desc}</p>
-                </div>
-            </div>
-        </div>`
-        )
-
-}
-    
-}
-
-export default Card;
