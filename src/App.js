74682ec490b17d3c83550c842fa92d0bb8a59ed7
NurKevser <nurkevserkadiroglu@gmail.com>
2021-08-25 14:22:05 +0300

    added onMouseClick event with props

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./components/navbar/Navbar";
 import Header from "./components/header/Header";
 import Card from "./components/cards/Card";
 import './App.css';
 
 const App =() => {
+const [isVisible, setIsVisible] = useState(true);
+
+const onMouseClick = () => {
+  setIsVisible(!isVisible);
+};
+
   return (
     <div className="App">
       <Navbar />
       <Header />
-      <Card />
+      <Card isVisible={isVisible} onMouseClick={onMouseClick} />
     </div>
   );
 }
--- src/components/cards/Card.js
+++ src/components/cards/Card.js
@@ -2,40 +2,29 @@ import React from "react";
 import { data } from "../../helpers/Data";
 import "./Card.css";
 
-function Card() {
-  return Datas();
-}
+function Card(props) {
+  return (
+    <div className='card-container'>
+      {data.map((card) => {
+        return (
+          <div className='cards' onClick={props.onMouseClick} >
+            <div className='title'>
+              <h2>{card.title}</h2>
+            </div>
 
-function Datas() {
-    
-  for (let i = 0; i < data.length; i++) {
-    return (
-     <div className="card-container">
-        <div className="cards">
-          <div className="title">
-            <h2>{data[i].title}</h2>
+            <img src={card.image} alt={card.title} />
+
+            <div className={props.isVisible ? 'card-over' : 'card-over-no'}>
+              <p>{card.desc}</p>
+            </div>
           </div>
-          <img src={data[i].image} alt={data[i].title} />
-          <div className="card-over">
-            <p>{data[i].desc}</p>
-          </div>
-        </div>
-      </div>
-        
-    );
-}
-}
+
+        );
+      })}
+
+    </div>
+  );
+};
 
 export default Card;
 
-/*  <div className="card-container">
-            <div className="cards">
-                <div className="title">
-                <h2>{data[0].title}</h2>
-                </div>
-                <img src={data[0].image} alt={data[0].title} />
-                <div className="card-over">
-                    <p>{data[0].desc}</p>
-                </div>
-            </div>
-        </div> */
